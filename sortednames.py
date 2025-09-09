ffb1c35912d1fb773d359376ca80c72586da3d9e
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:22:06 +0530

    Delete sortednames.py

--- sortednames.py
+++
@@ -1,7 +0,0 @@
-n = int(input("Enter number of names: "))
-names = []
-for i in range(n):
-    name = input(f"Enter name {i+1}: ")
-    names.append(name)
-names.sort()
-print("Sorted names:", names)
