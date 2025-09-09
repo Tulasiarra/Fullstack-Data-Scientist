e54e7c49e3052854be8058b1a27c540938d03ecb
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:17:54 +0530

    Delete largestfrom3numbers.py

--- largestfrom3numbers.py
+++
@@ -1,8 +0,0 @@
-nums = []
-for i in range(3):
-    nums.append(int(input(f"Enter number {i+1}: ")))
-largest = nums[0]
-for num in nums:
-    if num > largest:
-        largest = num
-print("Largest number is:", largest)
