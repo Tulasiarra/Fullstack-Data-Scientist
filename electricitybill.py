767373148be214e1c494ef5dac613654432dece4
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:16:17 +0530

    Delete electricitybill.py

--- electricitybill.py
+++
@@ -1,9 +0,0 @@
-units = int(input("Enter units consumed: "))
-bill = 0
-if units <= 100:
-    bill = units * 5
-elif units <= 200:
-    bill = 100*5 + (units-100)*7
-else:
-    bill = 100*5 + 100*7 + (units-200)*10
-print("Total bill: ₹", bill)
