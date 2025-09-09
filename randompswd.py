23a55f761435e29dc7c4db27ee91c2dafd16dfaf
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 06:07:50 +0530

    Delete randompswd.py

--- randompswd.py
+++
@@ -1,6 +0,0 @@
-import random, string
-def generate_password(length):
-    chars = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(chars) for _ in range(length))
-l=int(input())
-print(generate_password(l)) 
