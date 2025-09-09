2dd159fd76b4d08c7520e83ad0c755fcb74dbb18
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:21:33 +0530

    Delete simpleinterst.py

--- simpleinterst.py
+++
@@ -1,7 +0,0 @@
-def simple_interest(principal, rate, time):
-    return (principal * rate * time) / 100
-p = float(input("Enter Principal amount: "))
-r = float(input("Enter Rate of interest (%): "))
-t = float(input("Enter Time (in years): "))
-si = simple_interest(p, r, t)
-print("Simple Interest =", si)
