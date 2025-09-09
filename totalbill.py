1faf8e769a264dd9b533f6fc29ebce0e98eb7c99
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:23:06 +0530

    Delete totalbill.py

--- totalbill.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter number of items: "))
-items = {}
-for i in range(n):
-    name = input(f"Enter name of item {i+1}: ")
-    price = float(input(f"Enter price of {name}: "))
-    items[name] = price
-total = sum(items.values())
-print("Items:", items)
-print("Total bill: ₹", total)
