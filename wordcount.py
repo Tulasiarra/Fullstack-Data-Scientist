798c55cff7db0b928685a1078f041bbf5759d09a
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 22:23:54 +0530

    Delete wordcount.py

--- wordcount.py
+++
@@ -1,9 +0,0 @@
-with open("file.txt","w")as f:
-    f.write("HELLO CVR")
-def wordcount(file):
-    f=open(file,"r")
-    txt=f.read()
-    f.close()
-    return len(txt.split())
-print(wordcount("file.txt"))
-  
