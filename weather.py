425077ecbbbd158453382c1fc3270bfcc3e1b220
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 06:08:45 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,6 +0,0 @@
-import random
-def simulate_weather():
-    weather = random.choice(["Sunny", "Rainy", "Cloudy", "Windy"])
-    temp = random.randint(20, 40)
-    print(f"Weather: {weather}, Temp: {temp}°C")
-simulate_weather()
