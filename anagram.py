fd26c9490029caf8366475da623e7ad4fcac444f
Tulasiarra <tulasiarra1605@gmail.com>
2025-09-09 06:04:11 +0530

    Delete anagram.py

--- anagram.py
+++
@@ -1,7 +0,0 @@
-def is_anagram(word1, word2):
-    w1 = word1.replace(" ", "").lower()
-    w2 = word2.replace(" ", "").lower()
-    return sorted(w1) == sorted(w2)
-a=input()
-b=input()
-print(is_anagram(a,b)) 
